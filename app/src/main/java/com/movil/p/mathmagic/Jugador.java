8e7dd856ca86724b1bde949c007926443e648085
minivelx <minivelx@gmail.com>
2016-09-25 12:16:31 -0500

    creación clase jugador

---
+++ app/src/main/java/com/movil/p/mathmagic/Jugador.java
@@ -0,0 +1,50 @@
+package com.movil.p.mathmagic;
+
+
+public class Jugador {
+
+    private String nick;
+    private int nivel;
+    private int total_pts;
+    private int edad;
+
+    public Jugador() {
+        nick = null;
+        nivel = 1;
+        total_pts = 0;
+        edad = 6;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public void setTotal_pts(int total_pts) {
+        this.total_pts = total_pts;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public int getTotal_pts() {
+        return total_pts;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+}
