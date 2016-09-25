aeb0d9852714021c40c103487f5d8e39fa99efe3
minivelx <minivelx@gmail.com>
2016-09-25 12:26:19 -0500

    creación clase juego

---
+++ app/src/main/java/com/movil/p/mathmagic/Juego.java
@@ -0,0 +1,25 @@
+package com.movil.p.mathmagic;
+
+public class Juego {
+
+    private int subnivel;
+
+    public Juego() {
+        subnivel = 0;
+    }
+
+    public static String getTematica(int nivel){
+
+        switch (nivel){
+
+            case 1:
+                return null;
+
+
+        }
+
+        return null;
+
+    }
+
+}
